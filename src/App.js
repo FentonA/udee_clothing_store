b08e1a8d551de9fe9d41edf383a444adc8e2919c
Alf Fenton <Fentonalf@gmail.com>
2020-07-13 21:21:07 -0400

    Implemented Sign in compoenents, implemented user creationa and authentication using firebase

--- src/App.js
+++ src/App.js
@@ -4,7 +4,7 @@ import Header from './components/header/header.component';
 import Shop from './pages/shop/shop.component';
 import SignInAndSignUp from './pages/sign-in-and-sign-up/sign-in-and-sign-up.component'
 import {Switch, Route, BrowserRouter} from 'react-router-dom';
-import {auth} from './firebase/firebase.util'
+import {auth, creatUserProfileDocument} from './firebase/firebase.util'
 import './App.css';
 
 
@@ -21,8 +21,22 @@ class App extends React.Component{
   unsubscribeFromAuth = null 
 
   componentDidMount(){
-    auth.onAuthStateChanged(user =>{
-      this.setState({ currentUser: user});
+
+    this.unsubscribeFromAuth = auth.onAuthStateChanged( async userAuth =>{
+      if (userAuth){
+        const userRef = await creatUserProfileDocument(userAuth);
+
+        userRef.onSnapshot(snapshot=>{
+          this.setState({
+            currentUser: {
+              id: snapshot.id,
+              ...snapshot.data()
+            }
+          });
+          console.log(this.state);
+        });
+      }
+      this.setState({currentUser: userAuth});
     })
   }
 
